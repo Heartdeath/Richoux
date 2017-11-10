d87fa50281ac310694cd76115a66cadc46e57221
Thibault <thibault.beillevaire@gmail.com>
2017-11-10 14:23:44 +0100

    final + javadoc

---
+++ exo2/src/apiBasNiveau.java
@@ -0,0 +1,57 @@
+/**
+ * Classe BufferCirculaireBasNiveau
+ * 
+ * @author Beillevaire Thibault & Biermann Pierre
+ */
+public class apiBasNiveau<T> extends AbstractFileBloquanteBornee<T> {
+
+	/**
+	 * Constructeur 
+	 * @param n nombre d'objets acceptés
+	 */
+	public apiBasNiveau(int n) throws IllegalArgumentException {
+		super(n);
+	}
+
+	/**
+	 * Méthode déposer qui permet d'ajouter un objet dans la file. 
+	 * @param T objet à déposer
+	 */
+	public synchronized void deposer(T obj) {
+		int taille = tableau.length;
+
+		while (nombre == taille) { // si plein
+			try {
+				wait(); // attends non-plein
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		tableau[tete] = obj;
+		nombre++;
+		tete = (tete + 1) % taille;
+		notify(); // envoie un signal non-vide
+	}
+
+	/**
+	 * Méthode déposer qui permet de prendre un objet dans la file. 
+	 * @param T objet à prendre
+	 */
+	public synchronized T prendre() {
+		int taille = tableau.length;
+		while (nombre == 0) { // si vide
+			try {
+				wait(); // attends non-vide
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		T obj = tableau[queue];
+		tableau[queue] = null; // supprime la ref a l'objet
+		nombre--;
+		queue = (queue + 1) % taille;
+		notify(); // envoie un signal non-plein
+		return obj;
+	}
+
+}
---
+++ exo2/src/apiHautNiveau.java
@@ -0,0 +1,75 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Classe BufferCirculaireHautNiveau
+ * 
+ * @author Beillevaire Thibault & Biermann Pierre
+ */
+public class apiHautNiveau<T> extends AbstractFileBloquanteBornee<T> {
+	
+	private final Lock lock = new ReentrantLock();
+	private final Condition nonPlein  = lock.newCondition(); 
+	private final Condition nonVide = lock.newCondition(); 
+	
+	/**
+	 * Constructeur
+	 * @param n nombre d'objets acceptés par la file
+	 */
+	public apiHautNiveau(int n) throws IllegalArgumentException {
+		super(n);
+	}
+
+	/**
+	 * Méthode déposer qui permet de prendre un objet dans la file. 
+	 * @param T objet à déposer
+	 */
+	@Override
+	public void deposer(T obj) throws InterruptedException {
+		this.lock.lock();
+		try {
+			int taille = tableau.length;
+			
+			while (nombre == taille) {
+				this.nonPlein.await();
+			}
+			
+			tableau[tete] = obj;
+			nombre++;
+			tete = (tete + 1) % taille;
+			
+			this.nonVide.signal();
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	/**
+	 * Méthode déposer qui permet de prendre un objet dans la file. 
+	 * @param T objet à prendre
+	 */
+	@Override
+	public T prendre() throws InterruptedException {		
+		this.lock.lock();
+		try {
+			while (nombre == 0) {
+				this.nonVide.await();
+			}
+			
+			int taille = tableau.length;
+			
+			T obj = tableau[queue];
+			tableau[queue] = null; // supprime la ref a l'objet
+			nombre--;
+			queue = (queue + 1) % taille;
+			
+			this.nonPlein.signal();
+			
+			return obj;
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+}
