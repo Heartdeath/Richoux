5b2caa0d6614632955f72f49fa97bb7c8ce98db9
Thibault <thibault.beillevaire@gmail.com>
2017-11-10 14:13:06 +0100

    ajout javadoc exo1

--- exo1/src/Philosophe.java
+++ exo1/src/Philosophe.java
@@ -1,3 +1,8 @@
+/**
+ * Classe Baguette
+ * 
+ * @author Beillevaire Thibault & Biermann Pierre
+ */	
 class Baguette
 {
   private boolean _prise = false;
@@ -27,19 +32,33 @@ class Baguette
     notifyAll();
   }
 }
-		
+/**
+ * Classe Philosophe
+ * 
+ * @return un philosophe
+ * @author Beillevaire Thibault & Biermann Pierre
+ */		
 public class Philosophe implements Runnable
 {
   private String _nom;
   private Baguette _bGauche, _bDroite;
-
+  /**
+   * Constructeur de la classe philosophe
+   * 
+   * @param n nom
+   * @param g Baguette gauche
+   * @param d Baguette droite
+   */
   public Philosophe( String n, Baguette g, Baguette d )
   {
     _nom = n;
     _bGauche = g;
     _bDroite = d;
   }
-
+/**
+* Méthode run qui définit ce que le thread doit éxécuter
+* 
+*/
   public void run() {
 		while (true) {
 			penser();
@@ -60,11 +79,18 @@ public class Philosophe implements Runnable
 			}
 		}
 	}
+/**
+ * Méthode manger qui fait manger un philosophe
+ * 
+ */
   final void manger() 
   {
     System.out.println( _nom + " mange." );
   }
-
+/**
+ * Méthode penser qui fait penser le philosophe
+ * 
+ */
   final void penser() 
   {
     System.out.println( _nom + " pense." );
